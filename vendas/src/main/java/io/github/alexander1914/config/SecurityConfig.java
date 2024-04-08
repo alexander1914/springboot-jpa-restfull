7ee3f08450ccdac4a146060656f3f3ffc02b080d
alexander1914 <alexander.oliveira99@gmail.com>
2024-04-08 12:47:03 +0100

    Implementando o spring securiry part 1

---
+++ vendas/src/main/java/io/github/alexander1914/config/SecurityConfig.java
@@ -0,0 +1,55 @@
+package io.github.alexander1914.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+//TODO: @EnableWebSecurity: é uma anotation do spring security para definir as configurações para esse objeto.
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    //TODO: é para fazer autenticação dos usuários do security
+    // Autenticação
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        //TODO: BCryptPasswordEncoder: é um algoritimo muito avançado em autênticação
+        // OBS: ele recebe uma senha e criptografa essa senha, ele sempre cria um hash diferente.
+        return new BCryptPasswordEncoder();
+    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //TODO: Implementando um configuração básica do objeto AuthenticationManagerBuilder
+        // OBS: neste caso em memória.
+        auth.inMemoryAuthentication()
+                .passwordEncoder(passwordEncoder())
+                .withUser("alexander")
+                .password(passwordEncoder().encode("1914"))
+                .roles("USER", "ADMIN");
+    }
+    //TODO: esse método é para definir toda a parte de autorição
+    // Autorização
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        //TODO: Implementando um configuração básica do objeto HttpSecurity
+        // OBS: Aplicando os controles de acesso.
+        httpSecurity
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/clientes/**")
+                .hasAnyRole("USER", "ADMIN")
+
+                .antMatchers("/api/pedidos/**")
+                .hasRole( "ADMIN")
+
+                .antMatchers("/api/produtos/**")
+                .hasAnyRole("USER", "ADMIN")
+
+                .and()
+                .httpBasic();//TODO: é uma forma simples para acessar por header passandos os parametros.
+                //.formLogin(); //TODO: VIA login
+    }
+}
